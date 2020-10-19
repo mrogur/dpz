32b2754ed9bebf22924f09c4a8dff935bb27212a
Paweł Wlazło <mrogur@gmail.com>
2020-10-19 20:14:37 +0200

    added setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-import setuptools
 from setuptools import setup
 
 setup(
     name='dpz',
     version='0.0.1',
-    py_modules=["bs", "config", "dpz", "git"],
-    package_dir={'': 'dpz'},
+    packages=['dpz', 'tests'],
     url='',
     license='MIT',
     author='mrogur',
     author_email='mrogur@gmail.com',
-    description='',
+    description='Code deposit tool'
 )
