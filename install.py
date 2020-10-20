e65cd16737d1d556a7d6a66f16366246fcf8390c
Paweł Wlazło <mrogur@gmail.com>
2020-10-20 21:51:20 +0200

    adding support for argparse

--- install.py
+++ install.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 from pathlib import Path
+#import argparse
 
-from dpz.config import ConfigToml, PROJECT_ROOT, set_project_root
+from dpz.config import ConfigToml, set_project_root
 from dpz.dpz import Depozyt
 
 set_project_root(Path(__file__).parent)
-
+# parser = argparse.ArgumentParser(description="Depozyt tool")
+# parser.add_argument("")
 cfg = ConfigToml()
 depozyt = Depozyt(cfg.load())
 depozyt.run()
